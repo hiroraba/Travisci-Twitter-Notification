c3c6c67a523dbb5182cb760029a757cb0781b0b9
hiroraba <xornelius0313@gmail.com>
2014-08-16 22:29:35 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Travisci::Twitter::Notification - It's new $module
+
+# SYNOPSIS
+
+    use Travisci::Twitter::Notification;
+
+# DESCRIPTION
+
+Travisci::Twitter::Notification is ...
+
+# LICENSE
+
+Copyright (C) hiroraba.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hiroraba <xornelius0313@gmail.com>
---
+++ lib/Travisci/Twitter/Notification.pm
@@ -0,0 +1,39 @@
+package Travisci::Twitter::Notification;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Travisci::Twitter::Notification - It's new $module
+
+=head1 SYNOPSIS
+
+    use Travisci::Twitter::Notification;
+
+=head1 DESCRIPTION
+
+Travisci::Twitter::Notification is ...
+
+=head1 LICENSE
+
+Copyright (C) hiroraba.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hiroraba E<lt>xornelius0313@gmail.comE<gt>
+
+=cut
+
